e0f6bd0088fa8773c203d041a860912ad842b42f
JuanSFL <molina98j@yahoo.com>
2023-02-28 20:07:24 -0500

    Added content to skills component along with styling

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import HtmlIcon from '@mui/icons-material/Html';
 
 function Skills() {
   return (
     <div className='skills'>
-   
+    <h1>Skills</h1>
+    <ul className='list'>
+      <li className='item'>
+        <h2> Front-end</h2>
+        <span> HTML, CSS, BootStrap, React.JS </span>
+      </li>
+      <li className='item'>
+        <h2> Back-end</h2>
+        <span> NodeJS, Express.js, MySQL, MongoDB</span>
+      </li>
+      <li className='item'>
+        <h2> Languages</h2>
+        <span> JavaScript</span>
+      </li>
+    </ul>
   </div>
   )
 }
 
 export default Skills
+
